b85b5a0556ab2a3bf70ec109c0663cb728c56f79
francu <untiflash@gmail.com>
2024-07-30 02:04:44 +0200

    deleted trash

--- firstTry/pong/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('pong/', views.pong, name='pong'),
-	path('pong/login/', views.login_view, name='login'),
-	path('pong/register/', views.register_view, name='register'),
-]
