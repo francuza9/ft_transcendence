3c7c3f8ea9254f8b35f8ab76eb775048e8c12919
mdev9 <devriesmarijn05@gmail.com>
2024-08-08 15:53:37 +0200

    tried fixing navbar

--- dockers/nginx/static/views/login.js
+++ dockers/nginx/static/views/login.js
@@ -2,5 +2,5 @@ import { replaceHTML } from '/static/src/js/utils.js';
 
 export async function Login()
 {
-	replaceHTML('/static/src/html/login.html', false); 
+	replaceHTML('/static/src/html/login.html', true); 
 }
