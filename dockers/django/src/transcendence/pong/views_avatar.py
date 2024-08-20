498ddd6f9b239c36ea06ac16ea4c0b3b832c1e73
jwadie-a <jwadie-a@student.42angouleme.fr>
2024-08-20 23:11:51 +0200

    github auth functional. Reworking account page html and js

--- dockers/django/src/transcendence/pong/views_avatar.py
+++ dockers/django/src/transcendence/pong/views_avatar.py
@@ -6,41 +6,41 @@ from .models import Profile
 @login_required
 @csrf_exempt
 def update_avatar(request):
-    if request.method == 'POST':
-        user_profile = Profile.objects.get(user=request.user)
+	if request.method == 'POST':
+		user_profile = Profile.objects.get(user=request.user)
 
-        if 'avatar' not in request.FILES:
-            return JsonResponse({'success': False, 'message': 'No file uploaded.'}, status=400)
-        
-        avatar_file = request.FILES['avatar']
+		if 'avatar' not in request.FILES:
+			return JsonResponse({'success': False, 'message': 'No file uploaded.'}, status=400)
+		
+		avatar_file = request.FILES['avatar']
 
-        # If there is an existing avatar, delete the old file
-        if user_profile.avatarUrl:
-            user_profile.avatarUrl.delete(save=False)
-        
-        # Assign the new file to the avatarUrl field
-        user_profile.avatarUrl = avatar_file
-        user_profile.save()
+		# If there is an existing avatar, delete the old file
+		if user_profile.avatarUrl:
+			user_profile.avatarUrl.delete(save=False)
+		
+		# Assign the new file to the avatarUrl field
+		# user_profile.avatarUrl = avatar_file
+		# user_profile.save()
+		user_profile.avatarUrl.save(avatar_file.name, avatar_file, save=True)
 
-        return JsonResponse({'success': True, 'avatarUrl': user_profile.avatarUrl.url})
+		return JsonResponse({'success': True, 'avatarUrl': user_profile.avatarUrl.url})
 
-    return JsonResponse({'success': False, 'message': 'Invalid request method.'}, status=405)
+	return JsonResponse({'success': False, 'message': 'Invalid request method.'}, status=405)
 
 @login_required
 @csrf_exempt
 def remove_avatar(request):
     if request.method == 'POST':
-        user_profile = Profile.objects.get(user=request.user)
+        user_profile = Profile.objects.get(user=request.user)  # Get the user profile
 
         # If there is an avatar, delete the file
         if user_profile.avatarUrl:
             user_profile.avatarUrl.delete(save=False)
-        
-        # Set the avatar field to None
-        user_profile.avatarUrl = None
-        user_profile.save()
+            user_profile.avatarUrl = None
+            user_profile.save()
 
-        # Optionally, send the default avatar URL back (e.g., '/static/default-avatar.png')
-        return JsonResponse({'success': True, 'avatarUrl': '/static/default-avatar.png'})
+            return JsonResponse({'success': True, 'avatarUrl': None})
+        else:
+            return JsonResponse({'success': False, 'message': 'No avatar to remove.'}, status=400)
 
     return JsonResponse({'success': False, 'message': 'Invalid request method.'}, status=405)
