3c8e0dbbc3cb980d9c52fe2973abe5c22d0560b9
francu <untiflash@gmail.com>
2024-09-01 16:12:20 +0200

    friend request done?

--- dockers/django/src/transcendence/pong/consumers_global.py
+++ dockers/django/src/transcendence/pong/consumers_global.py
@@ -70,9 +70,11 @@ class GlobalConsumer(AsyncWebsocketConsumer):
 			}))
 		if senderDB and userDB:
 			if not await sync_to_async(senderDB.friends.filter(id=userDB.id).exists)() \
+				and not await sync_to_async(userDB.sent_friend_requests.filter(id=senderDB.id).exists)() \
+				and not await sync_to_async(userDB.received_friend_requests.filter(id=senderDB.id).exists)() \
 				and not await sync_to_async(userDB.blocked_list.filter(id=senderDB.id).exists)() \
 				and not await sync_to_async(senderDB.blocked_list.filter(id=userDB.id).exists)():
-				
+				await sync_to_async(senderDB.sent_friend_requests.add)(userDB)
 				await userWS.send(text_data=json.dumps({
 					'type': 'friend_request',
 					'sender': self.username,
