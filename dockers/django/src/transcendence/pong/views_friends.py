9c85242c95449c7a71268316879fc356f0b031a1
jwadie-a <jwadie-a@student.42angouleme.fr>
2024-09-03 17:41:17 +0200

    minor mod

--- dockers/django/src/transcendence/pong/views_friends.py
+++ dockers/django/src/transcendence/pong/views_friends.py
@@ -10,23 +10,23 @@ logger = logging.getLogger(__name__)
 @login_required
 @csrf_exempt
 def get_friends(request):
-    try:
-        user = request.user
+	try:
+		user = request.user
 
-        friends = user.friends.all()
+		friends = user.friends.all()
 
-        friends_data = []
-        for friend in friends:
-            friends_data.append({
-                'username': friend.username,
-                'name': friend.profile.displayName,
-                'avatar': friend.profile.avatarUrl.url if friend.profile.avatarUrl else None,
-                })
+		friends_data = []
+		for friend in friends:
+			friends_data.append({
+				'username': friend.username,
+				'name': friend.profile.displayName,
+				'avatar': friend.profile.avatarUrl.url if friend.profile.avatarUrl else None,
+				})
 
-        return JsonResponse({'success': True, 'friends': friends_data}, status=200)
+		return JsonResponse({'success': True, 'friends': friends_data}, status=200)
 
-    except Exception as e:
-        return JsonResponse({'success': False, 'error': str(e)}, status=500)
+	except Exception as e:
+		return JsonResponse({'success': False, 'error': str(e)}, status=500)
     
 @login_required
 @csrf_exempt
