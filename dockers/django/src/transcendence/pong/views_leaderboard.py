152d4edc78c9389d81cb9c57ae105bd7e033ef4f
mdev9 <devriesmarijn05@gmail.com>
2024-09-08 15:40:12 +0200

    fixed a few issues

--- dockers/django/src/transcendence/pong/views_leaderboard.py
+++ dockers/django/src/transcendence/pong/views_leaderboard.py
@@ -3,32 +3,32 @@ from .models import CustomUser, Profile, Game
 from django.db.models import F, ExpressionWrapper, FloatField, Case, When, Value
 
 def leaderboard_win_rate(request):
-	profiles = Profile.objects.all()
+    profiles = Profile.objects.filter(user__is_guest=False)
 
-	profiles = profiles.annotate(
-		win_ratio=Case(
-			When(gamesPlayed=0, then=Value(0.0)),
-			default=ExpressionWrapper(
-				F('gamesWon') * 1.0 / F('gamesPlayed'),
-				output_field=FloatField()
-			)
-		)
-	)
+    profiles = profiles.annotate(
+        win_ratio=Case(
+            When(gamesPlayed=0, then=Value(0.0)),
+            default=ExpressionWrapper(
+                F('gamesWon') * 1.0 / F('gamesPlayed'),
+                output_field=FloatField()
+            )
+        )
+    )
 
-	profiles = profiles.order_by('-win_ratio')
+    profiles = profiles.order_by('-win_ratio')
 
-	leaderboard_data = []
-	for profile in profiles:
-		leaderboard_data.append({
-			'username': profile.user.username,
-			'avatarUrl': profile.avatarUrl.url if profile.avatarUrl else None,
-			'displayName': profile.displayName,
-			'gamesPlayed': profile.gamesPlayed,
-			'gamesWon': profile.gamesWon,
-			'win_ratio': profile.win_ratio,
-		})
+    leaderboard_data = []
+    for profile in profiles:
+        leaderboard_data.append({
+            'username': profile.user.username,
+            'avatarUrl': profile.avatarUrl.url if profile.avatarUrl else None,
+            'displayName': profile.displayName,
+            'gamesPlayed': profile.gamesPlayed,
+            'gamesWon': profile.gamesWon,
+            'win_ratio': profile.win_ratio,
+        })
 
-	return JsonResponse(leaderboard_data, safe=False)
+    return JsonResponse(leaderboard_data, safe=False)
 
 def match_history(request, username):
 	try:
